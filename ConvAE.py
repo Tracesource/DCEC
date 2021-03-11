37e0f77680f45e3c2b8a07bc7b174c35799d69f3
tracesource <751871305@qq.com>
2021-03-11 10:52:36 +0800

    note 3.11

--- ConvAE.py
+++ ConvAE.py
@@ -4,7 +4,7 @@ from keras.utils.vis_utils import plot_model
 import numpy as np
 
 
-def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):
+def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):  #构建自编码器模型
     model = Sequential()
     if input_shape[0] % 8 == 0:
         pad3 = 'same'
@@ -16,8 +16,8 @@ def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):
 
     model.add(Conv2D(filters[2], 3, strides=2, padding=pad3, activation='relu', name='conv3'))
 
-    model.add(Flatten())
-    model.add(Dense(units=filters[3], name='embedding'))
+    model.add(Flatten())    
+    model.add(Dense(units=filters[3], name='embedding'))   #中间层得到的特征嵌入
     model.add(Dense(units=filters[2]*int(input_shape[0]/8)*int(input_shape[0]/8), activation='relu'))
 
     model.add(Reshape((int(input_shape[0]/8), int(input_shape[0]/8), filters[2])))
@@ -35,41 +35,42 @@ if __name__ == "__main__":
     # setting the hyper parameters
     import argparse
     parser = argparse.ArgumentParser(description='train')
-    parser.add_argument('--dataset', default='usps', choices=['mnist', 'usps'])
-    parser.add_argument('--n_clusters', default=10, type=int)
-    parser.add_argument('--batch_size', default=256, type=int)
-    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--dataset', default='usps', choices=['mnist', 'usps'])   #选择数据集
+    parser.add_argument('--n_clusters', default=10, type=int)      #聚类数
+    parser.add_argument('--batch_size', default=256, type=int)    #batch大小
+    parser.add_argument('--epochs', default=200, type=int)       
     parser.add_argument('--save_dir', default='results/temp', type=str)
     args = parser.parse_args()
     print(args)
 
     import os
-    if not os.path.exists(args.save_dir):
+    if not os.path.exists(args.save_dir):   #保存路径不存在就创建一个
         os.makedirs(args.save_dir)
 
     # load dataset
-    from datasets import load_mnist, load_usps
+    from datasets import load_mnist, load_usps   #从datasets.py中加载数据集
     if args.dataset == 'mnist':
         x, y = load_mnist()
     elif args.dataset == 'usps':
         x, y = load_usps('data/usps')
 
     # define the model
-    model = CAE(input_shape=x.shape[1:], filters=[32, 64, 128, 10])
+    model = CAE(input_shape=x.shape[1:], filters=[32, 64, 128, 10])    #定义模型
     plot_model(model, to_file=args.save_dir + '/%s-pretrain-model.png' % args.dataset, show_shapes=True)
     model.summary()
 
     # compile the model and callbacks
     optimizer = 'adam'
-    model.compile(optimizer=optimizer, loss='mse')
-    from keras.callbacks import CSVLogger
+    model.compile(optimizer=optimizer, loss='mse')  #定义模型的优化器与损失函数
+    from keras.callbacks import CSVLogger     #回调函数，查看函数内部状态
     csv_logger = CSVLogger(args.save_dir + '/%s-pretrain-log.csv' % args.dataset)
 
     # begin training
     t0 = time()
     model.fit(x, x, batch_size=args.batch_size, epochs=args.epochs, callbacks=[csv_logger])
     print('Training time: ', time() - t0)
-    model.save(args.save_dir + '/%s-pretrain-model-%d.h5' % (args.dataset, args.epochs))
+    model.save(args.save_dir + '/%s-pre
+    train-model-%d.h5' % (args.dataset, args.epochs))
 
     # extract features
     feature_model = Model(inputs=model.input, outputs=model.get_layer(name='embedding').output)
